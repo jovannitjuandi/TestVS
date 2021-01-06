5dedf554a37cef19b7b3347da1224908829521e0
jovannitjuandi <jovanni.tjuandi@gmail.com>
2021-01-07 08:41:23 +1100

    Initial Commit

---
+++ Application1/Application1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+namespace Application1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("HELLO WORLD");
+
+            ReadLine();
+        }
+    }
+}
